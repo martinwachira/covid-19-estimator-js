ae6ec4224dad70469a9b4ff4c290483783d74269
martin-wachira <martinnwachira@gmail.com>
2020-04-13 14:42:11 +0300

    --fix

--- src/estimator.js
+++ src/estimator.js
@@ -1,18 +1,47 @@
+
 const covid19ImpactEstimator = (data) => {
-  const input = data;
-  const currentlyInfected = 0;
-  const severeCasesByRequestedTime = (15 * input.infectionsByRequestedTime) / 100;
-  input.infectionsByRequestedTime = currentlyInfected * 512;
-  input.totalHospitalBeds = (35 * severeCasesByRequestedTime) / 100;
+  const {
+    periodTypes,
+    timeToElapse,
+    reportedCases
+  } = data;
+
+  const impact = {};
+  const severeImpact = {};
+
+  impact.currentlyInfected = reportedCases * 10;
+  severeImpact.currentlyInfected = reportedCases * 50;
+
+  let time;
+
+  switch (periodTypes) {
+    case 'Months':
+      time = Math.trunc((timeToElapse * 30) / 3);
+      break;
+    case 'Weeks':
+      time = Math.trunc((timeToElapse * 7) / 3);
+      break;
+    case 'Days':
+      time = Math.trunc((timeToElapse) / 3);
+      break;
+    default:
+  }
+
+  //   if (periodTypes === 'Days') {
+  //     time = Math.trunc(timeToElapse / 3);
+  //   } else if (periodTypes === 'Weeks') {
+  //     time = Math.trunc((timeToElapse * 7) / 3);
+  //   } else if (periodTypes === 'Months') {
+  //     time = Math.trunc((timeToElapse * 30) / 3);
+  //   }
+
+  impact.infectionsByRequesteTime = impact.currentlyInfected * (2 * time);
+  severeImpact.infectionsByRequesteTime = severeImpact.currentlyInfected * (2 * time);
+
   return {
-    data: input,
-    impact: {
-      infectionscurrentByRequestedTime: input.reportedCases * 10
-    },
-    severeImpact: {
-      infectionsByRequestedTime: input.reportedCases * 50,
-      severeCasesByRequestedTime
-    }
+    data,
+    impact,
+    severeImpact
   };
 };
 
