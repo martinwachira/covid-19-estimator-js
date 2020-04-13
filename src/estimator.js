88b0cbc13d7d61bffe761ecc5c8b4fa9b4c3e6bb
martin-wachira <martinnwachira@gmail.com>
2020-04-13 12:46:37 +0300

    fix infections

--- src/estimator.js
+++ src/estimator.js
@@ -1,11 +1,18 @@
 const covid19ImpactEstimator = (data) => {
   const input = data;
+  const currentlyInfected = 0;
+  const severeCasesByRequestedTime = (15 * input.infectionsByRequestedTime) / 100;
+  input.infectionsByRequestedTime = currentlyInfected * 512;
+  input.totalHospitalBeds = (35 * severeCasesByRequestedTime) / 100;
   return {
     data: input,
     impact: {
-      currentlyInfected: data.reportedCases * 10
+      infectionscurrentByRequestedTime: input.reportedCases * 10
     },
-    severeImpact: {}
+    severeImpact: {
+      infectionsByRequestedTime: input.reportedCases * 50,
+      severeCasesByRequestedTime
+    }
   };
 };
 
