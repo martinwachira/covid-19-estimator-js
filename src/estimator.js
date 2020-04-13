8e27f94ac9d5b1d7cca1811da710163980f21c16
martin-wachira <martinnwachira@gmail.com>
2020-04-13 15:21:24 +0300

    --

--- src/estimator.js
+++ src/estimator.js
@@ -2,41 +2,51 @@
 const covid19ImpactEstimator = (data) => {
   const {
     periodTypes,
-    timeToElapse,
+    // timeToElapse,
     reportedCases
   } = data;
+
+  let { timeToElapse } = data;
+
+  // normalize days; check for weeks and months if used
+  if (periodTypes === 'months') timeToElapse = Math.trunc(timeToElapse * 30);
+  if (periodTypes === 'weeks') timeToElapse = Math.trunc(timeToElapse * 7);
+  // if (periodType === 'days') timeToElapse = Math.trunc(timeToElapse * 1);
+
+  const infectionsByRequesteTime = (currentlyInfected) => {
+    const timeFactor = Math.trunc(timeToElapse / 3);
+    return currentlyInfected * (2 ** timeFactor);
+  };
 
   const impact = {};
   const severeImpact = {};
 
   impact.currentlyInfected = reportedCases * 10;
+  impact.infectionsByRequestedTime = infectionsByRequesteTime(
+    impact.currentlyInfected
+  );
   severeImpact.currentlyInfected = reportedCases * 50;
+  severeImpact.infectionsByRequestedTime = infectionsByRequesteTime(
+    severeImpact.currentlyInfected
+  );
 
-  let time;
+  //   let timeFactor;
 
-  switch (periodTypes) {
-    case 'Months':
-      time = Math.trunc((timeToElapse * 30) / 3);
-      break;
-    case 'Weeks':
-      time = Math.trunc((timeToElapse * 7) / 3);
-      break;
-    case 'Days':
-      time = Math.trunc((timeToElapse) / 3);
-      break;
-    default:
-  }
-
-  //   if (periodTypes === 'Days') {
-  //     time = Math.trunc(timeToElapse / 3);
-  //   } else if (periodTypes === 'Weeks') {
-  //     time = Math.trunc((timeToElapse * 7) / 3);
-  //   } else if (periodTypes === 'Months') {
-  //     time = Math.trunc((timeToElapse * 30) / 3);
+  //   switch (periodTypes) {
+  //     case 'Months':
+  //       timeFactor = Math.trunc(timeToElapse * 30);
+  //       break;
+  //     case 'Weeks':
+  //       timeFactor = Math.trunc(timeToElapse * 7);
+  //       break;
+  //     // case 'Days':
+  //     //   time = Math.trunc((timeToElapse) / 3);
+  //     //   break;
+  //     default:
   //   }
 
-  impact.infectionsByRequesteTime = impact.currentlyInfected * (2 ** time);
-  severeImpact.infectionsByRequesteTime = severeImpact.currentlyInfected * (2 ** time);
+  //   impact.infectionsByRequesteTime = impact.currentlyInfected * (2 ** timeFactor);
+  //   severeImpact.infectionsByRequesteTime = severeImpact.currentlyInfected * (2 ** timeFactor);
 
   return {
     data,
